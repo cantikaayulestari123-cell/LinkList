1f49acd4f9478566ee15d7060312ffe3947b6bb1
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-23 11:36:16 +0700

    menambahkan fungsi listEmpty

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -51,6 +51,32 @@ public:
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
+            previous = current;
+            current = current->next;
         }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    };
+
+    bool listEmpty()
+    {
+        return (START == NULL);
     }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+};
+
+    
 }
